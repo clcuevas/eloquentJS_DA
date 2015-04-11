5eb7bfa9bd7a65cadaff697eee1f2eb3b72a74dd
Claudia Cuevas <omordn@gmail.com>
2015-04-11 11:13:17 -0700

    working script that returns average age for mother's giving birth

--- motherChild.js
+++ motherChild.js
@@ -10,6 +10,23 @@ ancestry.forEach(function(person) {
   byName[person.name] = person;
 });
 
-// Your code here.
+// var getPeopleWithMother = ancestry.filter(function(person) {
+// 	return byName[person.mother] != null;
+// });
+// console.log(getPeopleWithMother);
 
+// var getAgeMotherGaveBirth = getPeopleWithMother.map(function(person) {
+// 	return person.born - byName[person.mother].born;
+// });
+// console.log(getAgeMotherGaveBirth);
+
+// console.log(average(getAgeMotherGaveBirth));
+
+//person parameter being passed is the array
+var ageDifference = ancestry.filter(function(person) {
+	return byName[person.mother] != null; //verifies mother property is not null
+}).map(function(person) {
+	return person.born - byName[person.mother].born; //age mother gave birth
+});
 // → 31.2
+average(ageDifference);
